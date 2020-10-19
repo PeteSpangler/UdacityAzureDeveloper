8f1d0a9dd1450cd8b651b220c13d330ea23319e4
peter <63021506+PeteSpangler@users.noreply.github.com>
2020-10-19 15:48:04 -0700

    Added gitignore file, linked sql/storage from azure in config.py

--- Article CMS Project/config.py
+++ Article CMS Project/config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'articleimages'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'HU6ZcEIeJktERfbrrUIthdQD7n2AXDicoG+S2eXVJmgkw3QuG18oRKZJz72LLcIN9I8uKivGU4Hi5dJdDqdLLQ=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'spangler-article-cms.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'article_cms_blogs'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'spangler'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Balto1234'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
