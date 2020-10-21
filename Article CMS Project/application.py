5fb8cf23f9a4d7b50900e2f0d562cb813533e9cf
peter <63021506+PeteSpangler@users.noreply.github.com>
2020-10-21 15:11:24 -0700

    removed server settings from application.py

--- Article CMS Project/application.py
+++ Article CMS Project/application.py
@@ -6,9 +6,4 @@ from os import environ
 from FlaskWebProject import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, ssl_context='adhoc')
+    app.run()
