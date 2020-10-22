407aea6925154938c5e6ccebfc13f4ad8611ee51
peter <63021506+PeteSpangler@users.noreply.github.com>
2020-10-22 10:11:19 -0700

    Changed the app file back

--- Article CMS Project/app.py
+++ Article CMS Project/app.py
@@ -4,6 +4,10 @@ This script runs the FlaskWebProject application using a development server.
 
 from os import environ
 from FlaskWebProject import app
-
 if __name__ == '__main__':
-    app.run()
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, ssl_context='adhoc')
