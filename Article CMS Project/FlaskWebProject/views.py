be42ea3eff7412724140d6d48ac304b20d750a8d
peter <63021506+PeteSpangler@users.noreply.github.com>
2020-10-23 10:16:40 -0700

    made some changes, lets see what happens

--- Article CMS Project/FlaskWebProject/views.py
+++ Article CMS Project/FlaskWebProject/views.py
@@ -88,7 +88,7 @@ def authorized():
     if request.args.get('code'):
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = build_msal_app(cache=cache).acquire_token_by_authorization_code(
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
             redirect_uri=url_for('authorized', _external=True, _scheme='https'))
--- Article CMS Project/app.py
+++ Article CMS Project/app.py
@@ -5,6 +5,7 @@ This script runs the FlaskWebProject application using a development server.
 from os import environ
 from FlaskWebProject import app
 if __name__ == '__main__':
+    app.debug = True
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
