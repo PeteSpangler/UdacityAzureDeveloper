c7038797addbe9f8ab65adf0e8192a24aa83b0d6
petespangler <63021506+PeteSpangler@users.noreply.github.com>
2020-11-25 17:08:19 -0800

    Front End works, Back End integrated

--- Neighborly_APP/NeighborlyFrontEnd/settings.py
+++ Neighborly_APP/NeighborlyFrontEnd/settings.py
@@ -1,6 +1,6 @@
 #--------- Flask settings
-SERVER_HOST = 'https://neighbourlyapp.azurewebsites.net' # Update this for the appropriate front-end website when up
-SERVER_PORT = 5000
+SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
+SERVER_PORT = 8000
 FLASK_DEBUG = False # Do not use debug mode in prod
 
 # Flask-Restplus settings
