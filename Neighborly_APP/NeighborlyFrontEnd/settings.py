823f49b34d38df08a8fef92d368413271e6ff85f
petespangler <63021506+PeteSpangler@users.noreply.github.com>
2020-11-30 11:53:00 -0800

    changed api url in settings.py

--- Neighborly_APP/NeighborlyFrontEnd/settings.py
+++ Neighborly_APP/NeighborlyFrontEnd/settings.py
@@ -16,4 +16,4 @@ API_VERSION = 'v1'
 #API_URL = " https://neighborlyapi.azurewebsites.net/api/"
 
 # for local host if Azure functions served locally
-API_URL = "https://neighbourly.azurewebsites.net/api/"
+API_URL = "https://neighbourlyapp.azurewebsites.net/api/"
