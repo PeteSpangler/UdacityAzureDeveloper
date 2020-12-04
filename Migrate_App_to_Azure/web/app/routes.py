9c67eee7d04e06c2a52ce5e93bc8d99ba735baa1
petespangler <63021506+PeteSpangler@users.noreply.github.com>
2020-12-04 11:59:49 -0800

    completed all steps of project part 1 but need to troubleshoot web app

--- Migrate_App_to_Azure/web/app/routes.py
+++ Migrate_App_to_Azure/web/app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db, queue_client
 from datetime import datetime
-from app.models import Attendee, Conference, Notification
+from models import Attendee, Conference, Notification
 from flask import render_template, session, request, redirect, url_for, flash, make_response, session
 from azure.servicebus import Message
 from sendgrid import SendGridAPIClient
--- Migrate_App_to_Azure/web/config.py
+++ Migrate_App_to_Azure/web/config.py
@@ -4,18 +4,18 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
+    POSTGRES_URL="migrationudacity.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="postgres@migrationudacity" #TODO: Update value
+    POSTGRES_PW="Balto1234"   #TODO: Update value
+    POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://udazmigrate.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=roxKSIyI4xhNtWAsIU39sbEU+kt2K3Wl1kOOWuc7qVY=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
-    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+    SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
