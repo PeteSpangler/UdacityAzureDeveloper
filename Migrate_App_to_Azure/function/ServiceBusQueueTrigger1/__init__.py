1f871760b45e73c521789c8c43edc649458f6b14
petespangler <63021506+PeteSpangler@users.noreply.github.com>
2020-12-05 15:12:40 -0800

    update environment variables in azure functions

--- Migrate_App_to_Azure/function/ServiceBusQueueTrigger1/__init__.py
+++ Migrate_App_to_Azure/function/ServiceBusQueueTrigger1/__init__.py
@@ -1,8 +1,7 @@
+import azure.functions as func
 import logging
 import os
 from datetime import datetime
-
-import azure.functions as func
 import psycopg2
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
@@ -11,8 +10,9 @@ from sendgrid.helpers.mail import Mail
 def main(msg: func.ServiceBusMessage):
 
     notification_id = int(msg.get_body().decode('utf-8'))
-    logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
+    logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
 
+    dbconnect = psycopg2.connect(os.environ["MyDbConnection"])
     # TODO: Get connection to database
 
     try:
@@ -27,5 +27,5 @@ def main(msg: func.ServiceBusMessage):
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
     finally:
-        pass
+        dbconnect.close()
         # TODO: Close connection
