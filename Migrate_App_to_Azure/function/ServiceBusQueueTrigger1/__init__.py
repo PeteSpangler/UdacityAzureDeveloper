1fc1f158b5b1459d3d032bd8db14a843873c7a4a
petespangler <63021506+PeteSpangler@users.noreply.github.com>
2020-12-09 13:32:41 -0800

    added screenshots, modified function

--- Migrate_App_to_Azure/function/ServiceBusQueueTrigger1/__init__.py
+++ Migrate_App_to_Azure/function/ServiceBusQueueTrigger1/__init__.py
@@ -11,21 +11,33 @@ def main(msg: func.ServiceBusMessage):
 
     notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
-
-    dbconnect = psycopg2.connect(os.environ["MyDbConnection"])
-    # TODO: Get connection to database
-
+ 
+    # Done: Get connection to database
+    connection = psycopg2.connect(os.environ["MyDbConnection"])
+    cursor = connection.cursor()
     try:
         # TODO: Get notification message and subject from database using the notification_id
-
+        notification_message = cursor.execute("SELECT message FROM Notification WHERE id=notification_id;")
+        notification_subject = cursor.execute("SELECT subject FROM Notification WHERE id=notification_id")
         # TODO: Get attendees email and name
+        cursor.execute("SELECT first_name, last_name, email FROM Attendee;")
+        attendees = cursor.fetchall()
 
+        for attendee in attendees:
+            subject = '{}: {}'.format(attendee.first_name, notification_subject)
+            send_email(attendee.email, subject, notification_message)
+
+        notification_completed_date = datetime.utcnow()
+        notification_status = 'Notified {} attendees'.format(len(attendees))
+        connection.commit()
         # TODO: Loop through each attendee and send an email with a personalized subject
 
         # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
         pass
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
+        connection.rollback()
     finally:
-        dbconnect.close()
-        # TODO: Close connection
+        cursor.close()
+        connection.close()
+        # Done: Close connection
