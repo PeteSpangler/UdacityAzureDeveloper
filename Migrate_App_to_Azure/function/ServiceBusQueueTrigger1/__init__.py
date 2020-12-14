522bd57888e77f25ded8cdb192cb58864062223b
petespangler <63021506+PeteSpangler@users.noreply.github.com>
2020-12-14 13:23:18 -0800

    added psycopg commands, psycopg2 to requirements and screenshot of notifications post submission

--- Migrate_App_to_Azure/function/ServiceBusQueueTrigger1/__init__.py
+++ Migrate_App_to_Azure/function/ServiceBusQueueTrigger1/__init__.py
@@ -13,29 +13,28 @@ def main(msg: func.ServiceBusMessage):
     logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
  
     # Done: Get connection to database
-    connection = psycopg2.connect(os.environ["MyDbConnection"])
+    connection = psycopg2.connect(dbname="techconfdb", user="postgres@migrationudacity", password="Balto1234", host="migrationudacity.postgres.database.azure.com")
     cursor = connection.cursor()
     try:
         # TODO: Get notification message and subject from database using the notification_id
-        notification_query = cursor.execute("SELECT message, subject FROM notification WHERE id = {}".format(notification_id))
+        notification_query = cursor.execute("SELECT message, subject FROM notification WHERE id = {};".format(notification_id))
 
         # TODO: Get attendees email and name
         cursor.execute("SELECT first_name, last_name, email FROM attendee;")
         attendees = cursor.fetchall()
 
-        email_list = {}
         for attendee in attendees:
             Mail('{}, {}, {}'.format({'admin@techconf.com'}, {attendee[2]}, {notification_query}))
-            email_list.update(attendee)
 
         notification_completed_date = datetime.utcnow()
 
-        notification_status = 'Notified {} attendees'.format(len(email_list))
+        notification_status = 'Notified {} attendees'.format(len(attendees))
         
         # TODO: Loop through each attendee and send an email with a personalized subject
 
         # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
-        update_query = cursor.execute("UPDATE notification SET status = '{}', completed_date = '{}' where id = {}".format(notification_status, notification_completed_date, notification_id))
+        update_query = cursor.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(notification_status, notification_completed_date, notification_id))        
+
         connection.commit()
 
     except (Exception, psycopg2.DatabaseError) as error:
